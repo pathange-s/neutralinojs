ff0815972babfec3cdcc747ec5bb89ca736a0f36
pathange-s <sainatharao@gmail.com>
2022-05-09 17:59:22 +0530

    test3

--- scripts/bz.py
+++ scripts/bz.py
@@ -207,7 +207,6 @@ def compile(cmd):
     print(msg)
 
 def print_ascii_art():
-    print('runnnnnnnnnnnnnnnnnnnnnnnnnnnnnning')
     print('''
   ____        _ _     _ ______     _
  |  _ \      (_) |   | |___  /    (_)
